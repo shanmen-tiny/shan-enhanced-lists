de18c869f8d0f241f9432a17fcd4ded3a89b2fd7
ShanMen <shanmen.ng@tiny.cloud>
2022-09-07 08:45:35 +0800

    Initial commit on shan-enhanced-lists TinyMCE plugin.

---
+++ README.md
@@ -0,0 +1,3 @@
+# shanEnhancedLists TinyMCE Plugin
+
+Welcome stranger! This is a repo containing the shanEnhancedLists TinyMCE plugin.
---
+++ src/main/ts/Main.ts
@@ -0,0 +1,9 @@
+import Plugin from './Plugin';
+
+Plugin();
+
+/** *****
+ * DO NOT EXPORT ANYTHING
+ *
+ * IF YOU DO ROLLUP WILL LEAVE A `shan-enhanced-lists` GLOBAL ON THE PAGE
+ *******/
